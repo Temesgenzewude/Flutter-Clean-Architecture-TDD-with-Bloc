99e086b5d31c3415bdb8bbf7e4dc050e5dff49d3
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-10-08 19:48:38 +0300

    update test

--- test_dart/test.dart
+++ test_dart/test.dart
@@ -1,338 +1,200 @@
+import 'dart:convert';
+
 void main() {
-  final products = [
-    {
-      "_id": "6475ed8b5ebadafe05d578bb",
-      "product_name": "Workflow AI",
-      "product_logo":
-          "https://100093.pythonanywhere.com/media/productlogos/Workflow-AI-2.png",
-      "product_link":
-          "https://ll04-finance-dowell.github.io/workflowai.online/#",
-      "product_status": "paid",
-      "team_members_status": "enable",
-      "users_status": "enable",
-      "public_status": "enable",
-      "paid_members": [
-        {
-          "username": "Jazz3650",
-          "owner": "Jazz3650",
-          "first_name": "Jazz3650",
-          "org_name": "Jazz3650",
-          "status": "disable",
-          "invite_users": "false",
-          "invite_team_members": "false",
-          "invite_public": "false"
-        },
-        {
-          "username": "Nagarajuvuliki",
-          "owner": "Nagarajuvuliki",
-          "org_name": "Nagarajuvuliki",
-          "invite_users": "true",
-          "invite_team_members": "false",
-          "invite_public": "false",
-          "status": "disable"
-        }
-      ],
-      "unpaid_members": [
-        {
-          "username": "alexkafupil11",
-          "owner": "alexkafupil11",
-          "first_name": "alexkafupil11",
-          "org_name": "alexkafupil11",
-          "status": "enable",
-          "invite_users": "enable",
-          "invite_team_members": "enable",
-          "invite_public": "enable"
-        },
-        {
-          "username": "lav",
-          "owner": "lav",
-          "first_name": "lav",
-          "org_name": "lav",
-          "status": "enable",
-          "invite_users": "true",
-          "invite_team_members": "false",
-          "invite_public": "false"
-        }
-      ]
-    },
-    {
-      "_id": "6475ed915ebadafe05d578bf",
-      "product_name": "Living Lab Chat",
-      "product_logo": "/media/productlogos/Livinglab-chat-1.png",
-      "product_link": "https://100096.pythonanywhere.com/living-lab-support/",
-      "product_status": "paid",
-      "team_members_status": "disable",
-      "users_status": "enable",
-      "public_status": "disable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475ed975ebadafe05d578c3",
-      "product_name": "Digital Queue",
-      "product_logo": "/media/productlogos/Digital-Q-1.png",
-      "product_link": "",
-      "members": [],
-      "product_status": "enable",
-      "team_members_status": "enable",
-      "users_status": "enable",
-      "public_status": "disable",
-      "paid_members": [],
-      "unpaid_members": [
-        {
-          "username": "MuhammadEssa",
-          "owner": "MuhammadEssa",
-          "first_name": "MuhammadEssa",
-          "org_name": "MuhammadEssa",
-          "status": "enable",
-          "invite_users": "true",
-          "invite_team_members": "false",
-          "invite_public": "false"
-        }
-      ]
-    },
-    {
-      "_id": "6475ed9d7a41d3b4b2d57d1c",
-      "product_name": "Wifi QR Code",
-      "product_logo": "/media/productlogos/Wifi-QR-Code-1.png",
-      "product_link": "",
-      "product_status": "none",
-      "team_members_status": "enable",
-      "users_status": "enable",
-      "public_status": "disable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475eda65ebadafe05d578ce",
-      "product_name": "User Experience Live",
-      "product_logo": "/media/productlogos/UX-Live-1.png",
-      "product_link": "",
-      "product_status": "unpaid",
-      "team_members_status": "enable",
-      "users_status": "disable",
-      "public_status": "disable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475edad7a41d3b4b2d57d27",
-      "product_name": "Social Media Automation",
-      "product_logo": "/media/productlogos/Social-media-automation-1.png",
-      "product_link": "https://100007.pythonanywhere.com/",
-      "members": [],
-      "product_status": "paid",
-      "team_members_status": "disable",
-      "users_status": "enable",
-      "public_status": "disable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475edb87a41d3b4b2d57d2e",
-      "product_name": "Living Lab Scales",
-      "product_logo": "/media/productlogos/Scales-1.png",
-      "product_link": "",
-      "product_status": "none",
-      "team_members_status": "enable",
-      "users_status": "enable",
-      "public_status": "enable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475edc65ebadafe05d578e2",
-      "product_name": "Logo Scan",
-      "product_logo": "/media/productlogos/Logo-Scan-1.png",
-      "product_link": "",
-      "product_status": "all",
-      "team_members_status": "disable",
-      "users_status": "disable",
-      "public_status": "enable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475edcd7a41d3b4b2d57d3c",
-      "product_name": "Legalzard",
-      "product_logo": "/media/productlogos/Legalzard-1.png",
-      "product_link": "",
-      "product_status": "unpaid",
-      "team_members_status": "enable",
-      "users_status": "disable",
-      "public_status": "disable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475edd27a41d3b4b2d57d40",
-      "product_name": "Living Lab Maps",
-      "product_logo": "/media/productlogos/dowell-maps-1.png",
-      "product_link": "",
-      "product_status": "paid",
-      "team_members_status": "disable",
-      "users_status": "disable",
-      "public_status": "enable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475edd87a41d3b4b2d57d44",
-      "product_name": "Customer Experience",
-      "product_logo": "/media/productlogos/Customer-experience-1.png",
-      "product_link": "",
-      "product_status": "all",
-      "team_members_status": "enable",
-      "users_status": "enable",
-      "public_status": "enable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475ede05ebadafe05d578f6",
-      "product_name": "Living Lab Admin",
-      "product_logo": "/media/productlogos/Living-Lab-Admin-1.png",
-      "product_link": "https://100093.pythonanywhere.com",
-      "product_status": "none",
-      "team_members_status": "enable",
-      "users_status": "disable",
-      "public_status": "disable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475ede55ebadafe05d578fa",
-      "product_name": "Living Lab Monitoring",
-      "product_logo": "/media/productlogos/Living-Lab-Admin-2.png",
-      "product_link": "",
-      "product_status": "paid",
-      "team_members_status": "disable",
-      "users_status": "enable",
-      "public_status": "enable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475edeb5ebadafe05d578fe",
-      "product_name": "Live Stream Dashboard",
-      "product_logo": "/media/productlogos/live-dashboard-1.png",
-      "product_link": "",
-      "product_status": "enable",
-      "team_members_status": "enable",
-      "users_status": "enable",
-      "public_status": "enable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475edf27a41d3b4b2d57d57",
-      "product_name": "Sales Agent",
-      "product_logo": "/media/productlogos/Sales-agent-app.png",
-      "product_link": "",
-      "product_status": "all",
-      "team_members_status": "enable",
-      "users_status": "enable",
-      "public_status": "enable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475edf87a41d3b4b2d57d5b",
-      "product_name": "Permutation Calculator",
-      "product_logo": "/media/productlogos/Permutation-calculator.png",
-      "product_link": "https://100050.pythonanywhere.com/calculator/",
-      "product_status": "unpaid",
-      "team_members_status": "disable",
-      "users_status": "enable",
-      "public_status": "disable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475edfe7a41d3b4b2d57d5f",
-      "product_name": "Dowell Customer Support Centre",
-      "product_logo": "/media/productlogos/customer-support-centre.png",
-      "product_link": "https://100096.pythonanywhere.com/customer-support/",
-      "product_status": "enable",
-      "team_members_status": "enable",
-      "users_status": "enable",
-      "public_status": "enable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475ee047a41d3b4b2d57d63",
-      "product_name": "Secure Repositories",
-      "product_logo": "/media/productlogos/secure-repositories.png",
-      "product_link": "",
-      "product_status": "paid",
-      "team_members_status": "enable",
-      "users_status": "enable",
-      "public_status": "enable",
-      "paid_members": [],
-      "unpaid_members": []
-    },
-    {
-      "_id": "6475ee097a41d3b4b2d57d67",
-      "product_name": "Secure Data",
-      "product_logo": "/media/productlogos/secure-data.png",
-      "product_link": "",
-      "product_status": "unpaid",
-      "team_members_status": "disable",
-      "users_status": "enable",
-      "public_status": "disable",
-      "paid_members": [],
-      "unpaid_members": []
-    }
-  ];
+  String query = "Test";
 
-  final productslst = products.map((product) {
-    dynamic productName = product['product_name'];
-    String productId = product["_id"].toString();
-
-    return {productId: productName};
-  }).toList();
-
-  final productsJson = {
-    "products": [
+  Map<String, dynamic> data = {
+    "isSuccess": true,
+    "data": [
       {
-        "_id": "6475ee047a41d3b4b2d57d63",
-        "product_name": "Secure Repositories",
-        "product_logo": "/media/productlogos/secure-repositories.png",
-        "product_link": "",
-        "product_status": "paid",
-        "team_members_status": "enable",
-        "users_status": "enable",
-        "public_status": "enable",
-        "paid_members": [],
-        "unpaid_members": []
+        "_id": "64efd37dad47b7f19d78029c",
+        "eventId": {
+          "is_success": true,
+          "event_id": "FB10100000001693438844#8169327",
+          "inserted_id": "64efd37cad47b7f19d78029b"
+        },
+        "DowellGroup": {
+          "group_name": "Sample Group 2",
+          "user_id": "456",
+          "created_by": "worlako",
+          "org_id": "6390b313d77dc467630713f2",
+          "org_name": "worlako",
+          "created_at_position": "my-channel-app",
+          "email_list": ["sample1@gmail.com", "sample2@gmail.com"],
+          "created_at": "2023-08-30T23:40:19.077782",
+          "deleted": false
+        }
       },
       {
-        "_id": "6475ee097a41d3b4b2d57d67",
-        "product_name": "Secure Data",
-        "product_logo": "/media/productlogos/secure-data.png",
-        "product_link": "",
-        "product_status": "unpaid",
-        "team_members_status": "disable",
-        "users_status": "enable",
-        "public_status": "disable",
-        "paid_members": [],
-        "unpaid_members": []
+        "_id": "64efd37dad47b7f19d78029c",
+        "eventId": {
+          "is_success": true,
+          "event_id": "FB10100000001693438844#8169327",
+          "inserted_id": "64efd37cad47b7f19d78029b"
+        },
+        "DowellGroup": {
+          "group_name": "Sample 2 Test",
+          "user_id": "456",
+          "created_by": "worlako",
+          "org_id": "6390b313d77dc467630713f2",
+          "org_name": "worlako",
+          "created_at_position": "my-channel-app",
+          "email_list": ["sample1@gmail.com", "sample2@gmail.com"],
+          "created_at": "2023-08-30T23:40:19.077782",
+          "deleted": false
+        }
+      },
+      {
+        "_id": "64efd6fdad47b7f19d7802a4",
+        "eventId": {
+          "is_success": true,
+          "event_id": "FB10100000001693439741#0050406",
+          "inserted_id": "64efd6fdad47b7f19d7802a3"
+        },
+        "DowellGroup": {
+          "group_name": "Sample Group 3 Test",
+          "user_id": "456",
+          "created_by": "worlako",
+          "org_id": "6390b313d77dc467630713f2",
+          "org_name": "worlako",
+          "created_at_position": "my-channel-app",
+          "email_list": ["sample1@gmail.com", "sample2@gmail.com"],
+          "created_at": "2023-08-30T23:55:15.367242",
+          "deleted": false
+        }
+      },
+      {
+        "_id": "64efda737fedf01bfb8d95bb",
+        "eventId": {
+          "is_success": true,
+          "event_id": "FB101000000001693440627#568946",
+          "inserted_id": "64efda737fedf01bfb8d95ba"
+        },
+        "DowellGroup": {
+          "group_name": "Sample Group 4",
+          "user_id": "456",
+          "created_by": "worlako",
+          "org_id": "6390b313d77dc467630713f2",
+          "org_name": "worlako",
+          "created_at_position": "my-channel-app",
+          "email_list": ["sample1@gmail.com", "sample2@gmail.com"],
+          "created_at": "2023-08-31T00:10:27.567856",
+          "deleted": false
+        }
+      },
+      {
+        "_id": "64f4c88b56531a1d9d89c247",
+        "eventId": {
+          "is_success": true,
+          "event_id": "FB101000000001693763723#609613",
+          "inserted_id": "64f4c88b19060c50a85115ff"
+        },
+        "DowellGroup": {
+          "group_name": "Sample  5",
+          "user_id": "456",
+          "created_by": "worlako",
+          "org_id": "6390b313d77dc467630713f2",
+          "org_name": "worlako",
+          "created_at_position": "my-channel-app",
+          "email_list": ["sample1@gmail.com", "sample2@gmail.com"],
+          "created_at": "2023-09-03T17:55:23.587512",
+          "deleted": false
+        }
       }
     ]
   };
-  // add new product
-  productsJson['products']?.add({
-    "_id": "testId",
-    "product_name": "Test Product",
-    "product_logo": "",
-    "product_link": "",
-    "product_status": "unpaid",
-    "team_members_status": "disable",
-    "users_status": "enable",
-    "public_status": "disable",
-    "paid_members": [],
-    "unpaid_members": []
-  });
-  print(productsJson['products']);
+
+  searchGroupsV1(query, data);
+}
+
+void searchGroupsV1(String query, Map<String, dynamic> jsonData) {
+  final List<dynamic> data = jsonData['data'];
+
+  // Filter the data list based on the query
+  List<dynamic> filteredGroups = data.where((group) {
+    Map<String, dynamic>? groupMap = group as Map<String, dynamic>?;
+    String? groupName =
+        groupMap?["DowellGroup"]?["group_name"]?.toString().toLowerCase();
+    return groupName?.contains(query.toLowerCase()) ?? false;
+  }).toList();
+
+  // Do something with the filteredGroups, such as updating the UI
+  // or storing the results in a new variable.
+
+  print(filteredGroups);
+}
+
+void searchGroups(String query) {
+  final data = [
+    {
+      "_id": "64f6a77af8366cc1a22dcf28",
+      "eventId": {
+        "is_success": true,
+        "event_id": "FB10100000001693886329#7180665",
+        "inserted_id": "64f6a779492215a52e7af11b"
+      },
+      "DowellGroup": {
+        "group_name": "Group Dev UPDATED",
+        "user_id": "6390b31ed77dc467630713f9",
+        "created_by": "WorkflowAi",
+        "org_id": "6390b313d77dc467630713f2",
+        "org_name": "HR_Dowell Research",
+        "created_at_position": "my-channel-app",
+        "email_list": ["dev1@gmail.com", "dev2@gmail.com"],
+        "created_at": "2023-09-05T03:58:49.712704",
+        "deleted": false
+      }
+    },
+    {
+      "_id": "64f6b88fc4a02ffba74d19d7",
+      "eventId": {
+        "is_success": true,
+        "event_id": "FB10100000001693890703#0615413",
+        "inserted_id": "64f6b88fb3cf55be27eed82c"
+      },
+      "DowellGroup": {
+        "group_name": "Group Marketing. UPDATED",
+        "user_id": "6390b31ed77dc467630713f9",
+        "created_by": "WorkflowAi",
+        "org_id": "6390b313d77dc467630713f2",
+        "org_name": "HR_Dowell Research",
+        "created_at_position": "my-channel-app",
+        "email_list": ["user3@gmail.com", "user1@gmail.com"],
+        "created_at": "2023-09-05T05:11:43.054226",
+        "deleted": false
+      }
+    },
+    {
+      "_id": "64f6b88fc4a02ffba74d19d7",
+      "eventId": {
+        "is_success": true,
+        "event_id": "FB10100000001693890703#0615413",
+        "inserted_id": "64f6b88fb3cf55be27eed82c"
+      },
+      "DowellGroup": {
+        "group_name": "Marketing. UPDATED",
+        "user_id": "6390b31ed77dc467630713f9",
+        "created_by": "WorkflowAi",
+        "org_id": "6390b313d77dc467630713f2",
+        "org_name": "HR_Dowell Research",
+        "created_at_position": "my-channel-app",
+        "email_list": ["user3@gmail.com", "user1@gmail.com"],
+        "created_at": "2023-09-05T05:11:43.054226",
+        "deleted": false
+      }
+    }
+  ];
+
+  // Filter the data list based on the query
+  List<dynamic> filteredGroups = data.where((group) {
+    Map<String, dynamic>? groupMap = group;
+    String? groupName =
+        groupMap["DowellGroup"]?["group_name"]?.toString().toLowerCase();
+    return groupName?.contains(query.toLowerCase()) ?? false;
+  }).toList();
+
+  print(filteredGroups);
+
+  // Do something with the filteredGroups, such as updating the UI
+  // or storing the results in a new variable.
 }
